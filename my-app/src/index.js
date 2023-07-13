7faf0082c00679ab2531f9238aae70ea7a878723
ImSeon1 <seonhyeogim01@gmail.com>
2023-07-13 23:59:35 +0900

    [2023-07-14] Chapter08

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,6 +8,7 @@ import Library from './chapter_03/Library'
 import Clock from './chapter_04/Clock'
 import CommentList from './chapter_05/CommentList';
 import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 /************
@@ -17,7 +18,7 @@ setInterval(()=>{
 */
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 
